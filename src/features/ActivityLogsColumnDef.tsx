7482f9878771c427e798af74796fb29576f97bfa
identiko <identiko@biporal>
2025-12-22 15:43:47 +0100

    small fix

--- src/features/ActivityLogsColumnDef.tsx
+++ src/features/ActivityLogsColumnDef.tsx
@@ -62,7 +62,7 @@ export const ActivityLogsColumns: ColumnDef<ActivitiyLog>[] = [
       <span
         className={`px-2 py-1 rounded text-sm ${
           row.original.isRejected
-            ? "bg-red-100 tect-red-700"
+            ? "bg-red-100 text-red-700"
             : "bg-green-100 text-green-700"
         }`}
       >
